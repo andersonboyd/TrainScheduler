c1660c8963878fab9d682be4442d7798b4c6b2f8
Anderson <andersonswaimboyd@gmail.com>
2019-06-12 08:15:01 -0500

    added html skeleton, reset.css, firebase initalizer in js

---
+++ logic.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDqGErsRRG15I3aB_CqbkfGdGikZW0AiGI",
+    authDomain: "trainscheduler-a2b86.firebaseapp.com",
+    databaseURL: "https://trainscheduler-a2b86.firebaseio.com",
+    projectId: "trainscheduler-a2b86",
+    storageBucket: "trainscheduler-a2b86.appspot.com",
+    messagingSenderId: "866300391516",
+    appId: "1:866300391516:web:81cd7c34eb443d5e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
