2d1d9e659c79fa5ec93f41a39b706d27b8051cad
Anderson <andersonswaimboyd@gmail.com>
2019-06-13 10:02:03 -0500

    added moment.js, currently debugging

--- logic.js
+++ logic.js
@@ -18,21 +18,13 @@ var trainFreq;
 var minutesAway;
 var nextArrival;
 
-function makeTable() {
-  var row = $("<tr>");
-  for (var i = 0; i < 5; i++) {
-      var td = $("<td>");
-      td.addClass("box-"+i);
-      row.append(td);
-  }
-  $("tbody").append(row);
-}
-
 database.ref().on("value", function(snapshot){
   var sv = snapshot.val();
   if(snapshot.child("trainName").exists() && snapshot.child("trainDest").exists()){
     trainName = sv.trainName;
     trainDest = sv.trainDest;
+    trainTime = sv.trainTime;
+    trainFreq = sv.trainFreq;
     console.log(trainName, trainDest);
   }else{
     console.log(trainName, trainDest);
@@ -64,18 +56,38 @@ $("#add-train").on("click", function(){
 
 database.ref().on("child_added", function(childSnapshot){
   var csv = childSnapshot.val();
-  console.log(csv.trainName, csv.trainDest, csv.trainTime, csv.trainFreq);
-  var newName = csv.trainName;
-  var newDest = csv.trainDest;
-  var newTime = csv.trainTime;
-  var newFreq = csv.trainFreq;
+  console.log(csv.name, csv.dest, csv.time, csv.freq);
+  var newName = csv.name;
+  var newDest = csv.dest;
+  var newTime = csv.time;
+  var newFreq = csv.freq;
+
+  var newTimeConverted = moment(newTime).subtract(1, "years");
+  console.log(newTimeConverted);
+
+  var currentTime = moment();
+  var currentTimeFormatted = moment(currentTime).format("hh:mm");
+  console.log("CURRENT TIME: "+ currentTimeFormatted);
+
+  var diffTime = moment(currentTimeFormatted).diff(moment(newTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: "+diffTime);
+
+  var remainder = diffTime % newFreq;
+  console.log(remainder);
+
+  minutesAway = newFreq - remainder;
+  console.log(minutesAway);
+
+  nextArrival = moment(currentTimeFormatted).add(minutesAway, "minutes");
+  console.log(nextArrival);
+
   $("#train-table > tbody").append(
     $("<tr>").append(
       $("<td>").text(newName),
       $("<td>").text(newDest),
       $("<td>").text(newFreq),
-      $("<td>"),
-      $("<td>")
+      $("<td>").text(nextArrival),
+      $("<td>").text(minutesAway)
     )
   );
 });
